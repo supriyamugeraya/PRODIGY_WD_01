96ddf7f0f1e33f3448888ea306330b279aeda928
supriyamugeraya <mugerayasupriya@gmail.com>
2024-07-09 20:57:24 +0530

    updated index.html,script.js,style.css

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navbar = document.querySelector(".navbar");
-
-  // Function to handle scroll event
-  function handleScroll() {
-    if (window.scrollY > 50) {
-      navbar.style.backgroundColor = "#444";
-    } else {
-      navbar.style.backgroundColor = "#333";
-    }
-  }
-
-  // Event listener for scroll event
-  window.addEventListener("scroll", handleScroll);
-
-  // Optional: Add more scripts or functionalities here as needed
+document.getElementById("hamburger").addEventListener("click", function () {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("active");
 });
